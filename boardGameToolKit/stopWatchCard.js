a0c261f4e30c2be3aa2b433a72a8ba3aa23d2856
lyDevper <luckydekdee@gmail.com>
2022-06-20 05:33:54 +0700

    converted with babel

--- boardGameToolKit/stopWatchCard.js
+++ boardGameToolKit/stopWatchCard.js
@@ -1,60 +1,63 @@
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
 class SWatchCard {
-    static timeNow = new TimeDelta(0, 0, 0);
-    static isRunning = false;
+  static init() {
+    SWatchCard.draw1 = SVG('#sWatchClockSvg').size(200, 200);
+    SWatchCard.clock = new Clock2(SWatchCard.draw1, 100, 100, 82, 0, 0);
+    SWatchCard.updateTime();
+  }
 
-    static draw;
-    static clock;
+  static updateTime() {
+    let secValue = SWatchCard.timeNow.msValue / 1000;
+    $('#sWatchLbl').text(SWatchCard.timeNow.toString2());
+    SWatchCard.clock.updateClock(secValue / 60, secValue);
+  }
 
-    static {
-        $().ready(() => {
-            setTimeout(() => {
-                SWatchCard.init();
-            }, 20);
-        });
+  static startBtnHandler() {
+    if (SWatchCard.isRunning) {
+      SWatchCard.stopRunning();
+    } else {
+      SWatchCard.startRunning();
+    }
+  }
+
+  static startRunning() {
+    SWatchCard.isRunning = true;
+    $('#sWatchStartBtn').text('Pause');
+    SWatchCard.interval = setInterval(() => {
+      SWatchCard.timeNow.adds(new TimeDelta(0, 0, 0.010));
+      SWatchCard.updateTime();
+    }, 10);
+  }
+
+  static stopRunning() {
+    SWatchCard.isRunning = false;
+    $('#sWatchStartBtn').text('Start');
+    clearInterval(SWatchCard.interval);
+  }
+
+  static resetBtnHandler() {
+    if (SWatchCard.isRunning) {
+      SWatchCard.startBtnHandler();
+      return;
     }
 
-    static init() {
-        SWatchCard.draw1 = SVG('#sWatchClockSvg').size(200, 200);
-        SWatchCard.clock = new Clock2(SWatchCard.draw1, 100, 100, 82, 0, 0);
-        SWatchCard.updateTime();
-    }
+    SWatchCard.timeNow.setByhms(0, 0, 0);
+    SWatchCard.updateTime();
+  }
 
-    static updateTime() {
-        let secValue = SWatchCard.timeNow.msValue / 1000;
-        $('#sWatchLbl').text(SWatchCard.timeNow.toString2());
-        SWatchCard.clock.updateClock(secValue / 60, secValue);
-    }
-
-    static startBtnHandler() {        
-        if(SWatchCard.isRunning) {            
-            SWatchCard.stopRunning();
-        }
-        else {            
-            SWatchCard.startRunning();
-        }
-    }
-
-    static startRunning() {
-        SWatchCard.isRunning = true;
-        $('#sWatchStartBtn').text('Pause');
-        SWatchCard.interval = setInterval(() => {
-            SWatchCard.timeNow.adds(new TimeDelta(0, 0, 0.010));
-            SWatchCard.updateTime();
-        }, 10);
-    }
-    
-    static stopRunning() {
-        SWatchCard.isRunning = false;
-        $('#sWatchStartBtn').text('Start');
-        clearInterval(SWatchCard.interval);        
-    }
-
-    static resetBtnHandler() {
-        if(SWatchCard.isRunning) {
-            SWatchCard.startBtnHandler();
-            return
-        }
-        SWatchCard.timeNow.setByhms(0, 0, 0);
-        SWatchCard.updateTime();
-    }
 }
+
+_defineProperty(SWatchCard, "timeNow", new TimeDelta(0, 0, 0));
+
+_defineProperty(SWatchCard, "isRunning", false);
+
+_defineProperty(SWatchCard, "draw", void 0);
+
+_defineProperty(SWatchCard, "clock", void 0);
+
+$().ready(() => {
+  setTimeout(() => {
+    SWatchCard.init();
+  }, 20);
+});
--- boardGameToolKit/timerCard.js
+++ boardGameToolKit/timerCard.js
@@ -1,87 +1,83 @@
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
 class TimerCard {
-    static instc = new TimerCard(); // singleton
-    static startTime = new TimeDelta($('#timerInput').val());
-    static timeNow = new TimeDelta($('#timerInput').val());
-    static isRunning = false;
-    static {
-        $().ready(() => {
-            setTimeout(() => {
-                TimerCard.init();
-            }, 40);
-        });        
+  // singleton
+  constructor() {//---
+  }
+
+  static init() {
+    TimerCard.draw1 = SVG('#timerClockSvg').size(192, 192);
+    TimerCard.clock = new Clock(TimerCard.draw1, 96, 96, 76, 1);
+    TimerCard.updateClock();
+    $('#timerClockSvg').on('click', function () {
+      setTimeout(function () {
+        $('#timerInput').click();
+      }, 1);
+    });
+  }
+
+  static updateClock() {
+    TimerCard.clock.updateProgressArc(TimerCard.timeNow.msValue / TimerCard.startTime.msValue);
+  } //0000-01-01T12:00:00Z
+
+
+  static inputChangedHandler() {
+    TimerCard.startTime = new TimeDelta($('#timerInput').val());
+    TimerCard.timeNow = new TimeDelta($('#timerInput').val());
+    TimerCard.updateClock();
+    console.log(TimerCard.timeNow.toString());
+  }
+
+  static startBtnHandler() {
+    if (TimerCard.isRunning) {
+      TimerCard.stopTimer();
+    } else {
+      TimerCard.startTimer();
     }
+  }
 
-    constructor() {        
-        //---
-    }
+  static startTimer() {
+    if (TimerCard.timeNow.msValue <= 0) TimerCard.resetBtnHandler();
+    TimerCard.isRunning = true;
+    $('#timerStartBtn').text('Stop');
+    TimerCard.interval = setInterval(() => {
+      TimerCard.timeNow.subtracts(new TimeDelta(0, 0, 0.010));
+      $('#timerInput').val(TimerCard.timeNow.toString());
+      this.clock.updateProgressArc(TimerCard.timeNow.msValue / TimerCard.startTime.msValue);
 
-    static init() {        
-        TimerCard.draw1 = SVG('#timerClockSvg').size(192, 192);
-        TimerCard.clock = new Clock(TimerCard.draw1, 96, 96, 76, 1);
-        TimerCard.updateClock();
-
-        $('#timerClockSvg').on('click', function() {
-            setTimeout(function() {
-                $('#timerInput').click();
-            }, 1);
-        });
-    }
-
-    static updateClock() {        
-        TimerCard.clock.updateProgressArc(TimerCard.timeNow.msValue / TimerCard.startTime.msValue);
-    }
-
-    //0000-01-01T12:00:00Z
-    static inputChangedHandler() {
-        TimerCard.startTime = new TimeDelta($('#timerInput').val());
-        TimerCard.timeNow = new TimeDelta($('#timerInput').val());
-        TimerCard.updateClock();
-        console.log(TimerCard.timeNow.toString());
-    }
-
-    static startBtnHandler() {
-        if(TimerCard.isRunning) {            
-            TimerCard.stopTimer();
-        }
-        else {            
-            TimerCard.startTimer();
-        }
-
-    }
-
-    static startTimer() {
-        if(TimerCard.timeNow.msValue <= 0)
-            TimerCard.resetBtnHandler();
-
-        TimerCard.isRunning = true;
-        $('#timerStartBtn').text('Stop');
-            
-        TimerCard.interval = setInterval(() => {            
-            TimerCard.timeNow.subtracts(new TimeDelta(0, 0, 0.010));
-            $('#timerInput').val(TimerCard.timeNow.toString());
-            this.clock.updateProgressArc(TimerCard.timeNow.msValue / TimerCard.startTime.msValue);
-
-            if(TimerCard.timeNow.msValue <= 0) {
-                TimerCard.stopTimer();
-                TimerCard.resetBtnHandler();
-            }
-        }, 10);
-
-    }
-
-    static stopTimer() {
-        TimerCard.isRunning = false;
-        $('#timerStartBtn').text('Start');
-
-        clearInterval(TimerCard.interval);
-    }
-
-    static resetBtnHandler() {
+      if (TimerCard.timeNow.msValue <= 0) {
         TimerCard.stopTimer();
-        TimerCard.timeNow.setByValue(TimerCard.startTime.msValue);
-        $('#timerInput').val(TimerCard.timeNow.toString());
-        TimerCard.updateClock();
-    }
-}
+        TimerCard.resetBtnHandler();
+      }
+    }, 10);
+  }
 
-//----------------------------------------------------------------
+  static stopTimer() {
+    TimerCard.isRunning = false;
+    $('#timerStartBtn').text('Start');
+    clearInterval(TimerCard.interval);
+  }
+
+  static resetBtnHandler() {
+    TimerCard.stopTimer();
+    TimerCard.timeNow.setByValue(TimerCard.startTime.msValue);
+    $('#timerInput').val(TimerCard.timeNow.toString());
+    TimerCard.updateClock();
+  }
+
+} //----------------------------------------------------------------
+
+
+_defineProperty(TimerCard, "instc", new TimerCard());
+
+_defineProperty(TimerCard, "startTime", new TimeDelta($('#timerInput').val()));
+
+_defineProperty(TimerCard, "timeNow", new TimeDelta($('#timerInput').val()));
+
+_defineProperty(TimerCard, "isRunning", false);
+
+$().ready(() => {
+  setTimeout(() => {
+    TimerCard.init();
+  }, 40);
+});
